2f76eb69b25fb5dbd9372301c75e15fd98965f84
Jendrik Mueller <jendrik.mueller@fh-bielefeld.de>
2020-05-20 11:28:29 +0200

    grpc Route hinzugefügt

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const path = require('path')
 const Mali = require('mali')
 
-function sayHello (ctx) {
-    ctx.res = { message: 'Hello ' + ctx.req.name }
+function addPatient (ctx) {
+    ctx.res = { success: true }
   }
   
-function sayHi (ctx) {
-    ctx.res = { message: 'Hi ' + ctx.req.name }
-}
 
-const PROTO_PATH = path.resolve(__dirname, './proto/helloworld.proto')
+const PROTO_PATH = path.resolve(__dirname, './proto/patient.proto')
 const app = new Mali(PROTO_PATH, 'Greeter')
-app.use({ sayHello, sayHi })
+app.use({ addPatient })
 app.start('127.0.0.1:50051')
