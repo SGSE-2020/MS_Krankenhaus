5e62b5e798d6115d064dd2dab25b58306fcc7d41
Jendrik Mueller <jendrik.mueller@fh-bielefeld.de>
2020-06-04 15:16:10 +0200

    Anpassen der CI Dateien

--- client/src/app/patient.model.ts
+++
@@ -1,8 +0,0 @@
-export class Patient {
-    id: string;
-    station: string;
-    faculty: string;
-    symptomps: string;
-    diagnosis: string;
-    medication: string;
-}
---
+++ frontend/src/app/patient.model.ts
@@ -0,0 +1,8 @@
+export class Patient {
+    id: string;
+    station: string;
+    faculty: string;
+    symptomps: string;
+    diagnosis: string;
+    medication: string;
+}
