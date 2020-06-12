eae38fe32d0aa9ec30f927c50de20165d769e364
Jendrik Mueller <jendrik.mueller@fh-bielefeld.de>
2020-06-12 11:54:59 +0200

    Einrichten des Login mit firebase

---
+++ frontend/src/app/app.routing.ts
@@ -0,0 +1,41 @@
+import {ExtraOptions, RouterModule, Routes} from '@angular/router';
+import {AngularFireAuthGuard, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+
+
+import { PatientsComponent } from './patients/patients.component';
+import { HomeComponent } from './home/home.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { StaffComponent } from './staff/staff.component';
+import { PatientDetailsComponent } from './patient-details/patient-details.component';
+import { BillsComponent } from './bills/bills.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { LoginComponent } from './login/login.component';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
+const routes: Routes = [
+    {
+      path: '', component: HomeComponent, canActivate: [AngularFireAuthGuard], data: {authGuardPipe: redirectUnauthorizedToLogin}, children: [
+        {path: '', redirectTo: 'home', pathMatch: 'full'},
+        
+      ]
+    },
+    {path: 'home', component: HomeComponent},
+    { path: 'staff', component: StaffComponent },
+    { path: 'appointments', component: AppointmentsComponent },
+    { path: 'patients', component: PatientsComponent },
+    { path: 'patient/:patientId', component: PatientDetailsComponent },
+    { path: 'bills', component: BillsComponent },
+    { path: 'impressum', component: ImpressumComponent },
+    {path: 'login', component: LoginComponent, data: {authGuardPipe: redirectLoggedInToHome}},
+  
+    // If there is not match, redirect to home
+    {path: '**', redirectTo: ''}
+  ];
+
+const routerOptions: ExtraOptions = {
+  useHash: false,
+};
+
+export const AppRoutingModule = RouterModule.forRoot(routes, routerOptions);
--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthenticationService} from '../authentication.service';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public  authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.authenticationService.logout();
   }
 
 }
